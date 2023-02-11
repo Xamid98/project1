0d074e98ab2955d4d1affc76f52cf8a62970d57e
Hamid <xamid0700@gmail.com>
2023-02-11 15:13:23 +0500

    task1/2

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const NumberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
