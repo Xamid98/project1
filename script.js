0bc511ea3b4120f08c8fe840f6ee6c2e6b232f2c
Hamid <xamid0700@gmail.com>
2023-02-11 15:10:24 +0500

    task1/1

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const obj = {
-  name: 'Hamid',
-  sayHi() {
-    console.log(this.name);
-  },
-};
-
-// obj.sayHi
-
-const func = obj.sayHi;
-
-func.apply(obj);
+const NumberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
